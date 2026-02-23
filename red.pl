1b419c5760a588cacf5b2776201608e51e8ed595
Gerhard Gonter <ggonter@gmail.com>
2026-02-23 13:53:36 +0100

    handle different output formats, including "json" and "pretty"

--- red.pl
+++ red.pl
@@ -7,19 +7,27 @@ use strict;
 use Data::Dumper;
 $Data::Dumper::Indent= 1;
 
+use FileHandle;
+use JSON;
+
 use Redmine::CLI;
 
-  my $rm_cli= new Redmine::CLI(cfg_stanza => $ENV{'REDMINE'});
+  binmode (STDOUT, ':utf8');
+  binmode (STDERR, ':utf8');
+
+  my $rm_cli= Redmine::CLI->new(cfg_stanza => $ENV{REDMINE});
   # print __LINE__, " rm_cli: ", Dumper ($rm_cli);
 
   $rm_cli->parse_args(@ARGV);
+  # print __LINE__, " extra_options: ", Dumper($rm_cli->{extra_options});
 
   my ($cfg, $mRM)= $rm_cli->init();
   # print __LINE__, " rm_cli: ", Dumper ($rm_cli);
 
   my $res= $rm_cli->main_part2();
-  print -_LINE__, " rm_cli: res: ", Dumper($res);
-  print __LINE__, " rm_cli: ", Dumper ($rm_cli);
+  print __LINE__, " rm_cli: res: ", Dumper($res);
+  # print __LINE__, " rm_cli: ", Dumper ($rm_cli);
+  print encode_json($res);
 
 exit;
 
